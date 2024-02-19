f54ed1029c4f1fead5c602b29b1fecc919b77a43
augustobrigo <profeaugustobriga@gmail>
2024-02-19 12:51:27 +0100

    temporizadorAngular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PadreComponent } from './padre/padre.component';
+import { TemporizadorComponent } from './temporizador/temporizador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PadreComponent,
+    TemporizadorComponent
   ],
   imports: [
     BrowserModule,
